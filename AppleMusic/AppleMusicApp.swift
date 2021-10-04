c77c62249c5a902e73d4666164adf703ee56c02f
AlmiraEfremenko <almira.efremenko@yandex.ru>
2021-10-04 17:35:27 +0500

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by MAC on 04.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
