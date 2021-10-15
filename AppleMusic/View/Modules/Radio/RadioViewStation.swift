bb4f9bfe8227b086ab96eebee9d82ecfc4ed28c5
AlmiraEfremenko <almira.efremenko@yandex.ru>
2021-10-15 22:20:53 +0500

    Add radio view station().

--- AppleMusic/View/Modules/Radio/RadioViewStation.swift
+++ AppleMusic/View/Modules/Radio/RadioViewStation.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct RadioViewStation: View {
+    
+    @State private var modelsRadioStation = ModelRadioStation.data
+    
+    var columns = [
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LazyVGrid(columns: columns, alignment: .leading) {
+            Section(header: Text("Cтанции")
+                        .foregroundColor(.black)
+                        .fontWeight(.bold)
+                        .font(.system(size: 20))
+                        .padding(.horizontal, 10))
+            {
+                ForEach(modelsRadioStation, id: \.id) { modelRadio in
+                    HStack(spacing: 10) {
+                        Image(modelRadio.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100)
+                        VStack(alignment: .leading) {
+                            Text(modelRadio.title)
+                                .foregroundColor(.black)
+                                .fontWeight(.regular)
+                                .font(.system(size: 18))
+                            Text(modelRadio.subtitle)
+                                .foregroundColor(.secondary)
+                                .fontWeight(.regular)
+                                .font(.system(size: 14))
+                        }
+                    }
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 10)
+                    Divider()
+                }
+            }
+        }
     }
 }
 
