0e9a11a0b9e721dc3563d2e0a454aea131074049
AlmiraEfremenko <almira.efremenko@yandex.ru>
2021-10-15 22:19:18 +0500

    Add model radio().

---
+++ AppleMusic/View/Model/ModelRadioStation.swift
@@ -0,0 +1,20 @@
+//
+//  ModelRadioStation.swift
+//  AppleMusic
+//
+//  Created by MAC on 15.10.2021.
+//
+
+import SwiftUI
+
+struct ModelRadioStation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ModelRadioStation_Previews: PreviewProvider {
+    static var previews: some View {
+        ModelRadioStation()
+    }
+}
---
+++ AppleMusic/View/Modules/Radio/RadioViewPlaylist.swift
@@ -0,0 +1,20 @@
+//
+//  RadioViewPlaylist.swift
+//  AppleMusic
+//
+//  Created by MAC on 15.10.2021.
+//
+
+import SwiftUI
+
+struct RadioViewPlaylist: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RadioViewPlaylist_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioViewPlaylist()
+    }
+}
