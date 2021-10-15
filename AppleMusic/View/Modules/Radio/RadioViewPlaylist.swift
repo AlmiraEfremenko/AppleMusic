55fe2c9c0613e19b3f3983ef8f443de0b10c4814
AlmiraEfremenko <almira.efremenko@yandex.ru>
2021-10-15 22:20:34 +0500

    Add radio view playlist().

--- AppleMusic/View/Modules/Radio/RadioViewPlaylist.swift
+++ AppleMusic/View/Modules/Radio/RadioViewPlaylist.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct RadioViewPlaylist: View {
+    
+    @State private var modelsRadio = ModelRadio.data
+    
+    var rows = [
+        GridItem(.fixed(320))
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHGrid(rows: rows) {
+                ForEach(modelsRadio, id: \.id) { modelRadio in
+                    VStack(alignment: .leading) {
+                        Text(modelRadio.title)
+                            .foregroundColor(.secondary)
+                            .fontWeight(.medium)
+                            .font(.system(size: 16))
+                        Text(modelRadio.subtitle)
+                            .foregroundColor(.black)
+                            .fontWeight(.regular)
+                            .font(.system(size: 25))
+                        Spacer()
+                            .frame(height: 10)
+                        Image(modelRadio.image)
+                            .resizable()
+                            .frame(width: 370)
+                    }
+                }
+            }
+        }
     }
 }
 
