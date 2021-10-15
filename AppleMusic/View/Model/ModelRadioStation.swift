95b75a9dd9b9137f6d3592bfb89d8e98315ced69
AlmiraEfremenko <almira.efremenko@yandex.ru>
2021-10-15 22:19:55 +0500

    Add model radio station().

--- AppleMusic/View/Model/ModelRadioStation.swift
+++ AppleMusic/View/Model/ModelRadioStation.swift
@@ -7,14 +7,23 @@
 
 import SwiftUI
 
-struct ModelRadioStation: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+
+struct ModelRadioStation: Identifiable, Hashable {
+    let id = UUID()
+    var image: String
+    var title: String
+    var subtitle: String
 }
 
-struct ModelRadioStation_Previews: PreviewProvider {
-    static var previews: some View {
-        ModelRadioStation()
-    }
+extension ModelRadioStation {
+    
+    static let data = [
+        ModelRadio(image: "популярное", title: "Популярное", subtitle: "То, что слушают прямо сейчас."),
+        ModelRadio(image: "classic", title: "Классика", subtitle: "Выдающиеся образцы жанра."),
+        ModelRadio(image: "chill1", title: "Для расслабления", subtitle: "Электронная медитация."),
+        ModelRadio(image: "rock", title: "Классика рokа", subtitle: "Гитарный звук."),
+        ModelRadio(image: "hip-hop", title: "Хип-хоп", subtitle: "Идеальные биты."),
+        ModelRadio(image: "dfmdisco90", title: "Музыка 90-х", subtitle: "Зажигай."),
+        ModelRadio(image: "rnb", title: "RnB", subtitle: "Будь в движении.")
+    ]
 }
