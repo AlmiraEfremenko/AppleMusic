a7d614d7f293ee437c4cbfcc09af114d9cd912b5
AlmiraEfremenko <almira.efremenko@yandex.ru>
2021-12-17 14:14:09 +0500

    Add searchViewRepresentable().

--- AppleMusic/View/Modules/Search/SearchViewRepresentable.swift
+++ AppleMusic/View/Modules/Search/SearchViewRepresentable.swift
@@ -5,4 +5,16 @@
 //  Created by MAC on 16.12.2021.
 //
 
-import Foundation
+import SwiftUI
+import UIKit
+
+struct SearchViewRepresentable: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        return SearchViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
